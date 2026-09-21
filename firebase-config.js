94f4a7abe80933e9204160d9d439427a2afd9bc2
cikguadnan <adnan_mahmud@moe.edu.sg>
2026-09-21 15:09:17 +0800

    Add Portify Firebase web config

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Portify Firebase web configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAzWiVAWB8oYycGVXCoTC-K3Jg4iulFGas",
+  authDomain: "portifyapp01.firebaseapp.com",
+  projectId: "portifyapp01",
+  storageBucket: "portifyapp01.firebasestorage.app",
+  messagingSenderId: "149193324174",
+  appId: "1:149193324174:web:342fb3efbb89bda71d7bdf",
+  measurementId: "G-MSCCM0RXHN"
+};
